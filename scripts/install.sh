d72726f1768dc319d6e11ea189de588293fb26e9
root <stjbrown@gmail.com>
2018-01-10 16:27:16 +0000

    Updated Scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,11 +11,7 @@ pip install azure-cli
 #Add dev module bigip_node.py
 cd ~
 git clone https://github.com/F5Networks/f5-ansible -b devel
-mv /usr/lib/python2.7/site-packages/ansible/modules/network/f5/bigip_node.py bigip_node.py.old
-cp ~/f5-ansible/library/bigip_node.py /usr/lib/python2.7/site-packages/ansible/modules/network/f5/bigip_node.py
-
-mv /usr/lib/python2.7/site-packages/ansible/modules/network/f5/bigip_virtual_server.py bigip_virtual_server.py.old
-cp ~/f5-ansible/library/bigip_virtual_server.py /usr/lib/python2.7/site-packages/ansible/modules/network/f5/bigip_virtual_server.py
+cp ~/f5-ansible/library/. /usr/lib/python2.7/site-packages/ansible/modules/network/f5/
 
 #Add my git email for dev
 git config --global user.email "stjbrown@gmail.com"
