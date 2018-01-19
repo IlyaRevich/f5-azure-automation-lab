e47b5a7b6b30541a997ec73eff424aeedd6a4f2b
root <stjbrown@gmail.com>
2018-01-19 14:20:09 +0000

    Testing

--- scripts/fullBuild.sh
+++ scripts/fullBuild.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ansible-playbook ~/f5-azure-automation-lab/bigipClusterCreate/site.yml
+#ansible-playbook ~/f5-azure-automation-lab/bigipClusterCreate/site.yml
 
-ansible-playbook ~/f5-azure-automation/coreosCreate/site.yml
+ansible-playbook ~/f5-azure-automation-lab/coreosCreate/site.yml
 
-ansible-playbook -i ~/f5-azure-automation/coreosConfigure/azure_rm.py ~/f5-azure-automation/coreosConfigure/site.yml
+ansible-playbook -i ~/f5-azure-automation-lab/coreosConfigure/azure_rm.py ~/f5-azure-automation-lab/coreosConfigure/site.yml
 
-ansible-playbook -i ~/f5-azure-automation/f5HelloWorld/azure_rm.py ~/f5-azure-automation/f5HelloWorld/site.yml
+ansible-playbook -i ~/f5-azure-automation-lab/f5HelloWorld/azure_rm.py ~/f5-azure-automation-lab/f5HelloWorld/site.yml
